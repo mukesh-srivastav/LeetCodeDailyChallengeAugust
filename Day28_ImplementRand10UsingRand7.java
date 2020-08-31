ddd137ad662907a84c9fa9991ce1aceb3ec5aad2
Mukesh Srivastava <mukeshsrivastav691@gmail.com>
2020-09-01 01:59:38 +0530

    Day28 ImplementRand10UsingRand7.java

---
+++ Day28_ImplementRand10UsingRand7.java
@@ -0,0 +1,42 @@
+/**
+ * Given the API rand7 which generates a uniform random integer in the range 1 to 7, write a function rand10 which generates a uniform random integer in the range 1 to 10. You can only call the API rand7 and you shouldn't call any other API. Please don't use the system's Math.random().
+
+Notice that Each test case has one argument n, the number of times that your implemented function rand10 will be called while testing. 
+
+Follow up:
+
+What is the expected value for the number of calls to rand7() function?
+Could you minimize the number of calls to rand7()?
+ 
+
+Example 1:
+
+Input: n = 1
+Output: [2]
+Example 2:
+
+Input: n = 2
+Output: [2,8]
+Example 3:
+
+Input: n = 3
+Output: [3,8,10]
+ 
+
+Constraints:
+
+1 <= n <= 105
+ */
+public class Day28_ImplementRand10UsingRand7 {
+    private int m_last = 0;
+    
+    public int rand10() {
+        int liResult = rand7() + m_last;
+        if ( liResult > 10 )
+            liResult = liResult - 10;
+        
+        m_last = liResult;
+        
+        return liResult;
+    } 
+}
